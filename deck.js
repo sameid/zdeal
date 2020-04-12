26aced9c0e1b6c8cc2baf6a096b807e812abc67f
sameid <sameid.usmani@gmail.com>
2020-04-12 00:46:20 -0400

    ne wdeck

--- deck.js
+++ deck.js
@@ -82,37 +82,46 @@ let deck = [
     },
     {
         type: "property",
-        color1: "lightblue",
-        color2: null,
-        cost1: [1, 2, 3],
-        cost2: null,
-        value: 1,
-        actionType: null
-    }
-    {
-        type: "property",
-        color1: "lightblue",
-        color2: null,
-        cost1: [1, 2, 3],
-        cost2: null,
-        value: 1,
-        actionType: null
-    }
-    {
-        type: "property",
-        color1: "lightblue",
-        color2: null,
-        cost1: [1, 2, 3],
-        cost2: null,
-        value: 1,
-        actionType: null
-    }
-    {
-        type: "property",
         color1: "lightgreen",
         color2: "black",
         cost1: [1, 2],
         cost2: [1, 2, 3, 4],
+        value: 2,
+        actionType: null
+    },
+    {
+        type: "property",
+        color1: "lightblue",
+        color2: null,
+        cost1: [1, 2, 3],
+        cost2: null,
+        value: 1,
+        actionType: null
+    },
+    {
+        type: "property",
+        color1: "lightblue",
+        color2: null,
+        cost1: [1, 2, 3],
+        cost2: null,
+        value: 1,
+        actionType: null
+    },
+    {
+        type: "property",
+        color1: "lightblue",
+        color2: null,
+        cost1: [1, 2, 3],
+        cost2: null,
+        value: 1,
+        actionType: null
+    },
+    {
+        type: "property",
+        color1: "pink",
+        color2: null,
+        cost1: [1, 2, 4],
+        cost2: null,
         value: 2,
         actionType: null
     },
@@ -124,7 +133,7 @@ let deck = [
         cost2: null,
         value: 2,
         actionType: null
-    }
+    },
     {
         type: "property",
         color1: "pink",
@@ -133,16 +142,7 @@ let deck = [
         cost2: null,
         value: 2,
         actionType: null
-    }
-    {
-        type: "property",
-        color1: "pink",
-        color2: null,
-        cost1: [1, 2, 4],
-        cost2: null,
-        value: 2,
-        actionType: null
-    }
+    },
     {
         type: "property",
         color1: "pink",
@@ -151,7 +151,7 @@ let deck = [
         cost2: [1, 3, 5],
         value: 2,
         actionType: null
-    }
+    },
     {
         type: "property",
         color1: "pink",
@@ -160,7 +160,7 @@ let deck = [
         cost2: [1, 3, 5],
         value: 2,
         actionType: null
-    }
+    },
     {
         type: "property",
         color1: "brown",
@@ -169,7 +169,7 @@ let deck = [
         cost2: null,
         value: 1,
         actionType: null
-    }
+    },
     {
         type: "property",
         color1: "brown",
@@ -178,7 +178,7 @@ let deck = [
         cost2: null,
         value: 1,
         actionType: null
-    }
+    },
     {
         type: "property",
         color1: "brown",
@@ -187,14 +187,446 @@ let deck = [
         cost2: [1, 2, 3],
         value: 1,
         actionType: null
-    }
+    },
     {
         type: "property",
         color1: "blue",
         color2: null,
         cost1: [3, 8],
         cost2: null,
-        value: 1,
+        value: 4,
         actionType: null
-    }
+    },
+    {
+        type: "property",
+        color1: "blue",
+        color2: null,
+        cost1: [3, 8],
+        cost2: null,
+        value: 4,
+        actionType: null
+    },
+    {
+        type: "property",
+        color1: "blue",
+        color2: "green",
+        cost1: [3, 8],
+        cost2: [2, 4, 7]
+        value: 4,
+        actionType: null
+    },
+    {
+        type: "property",
+        color1: "green",
+        color2: null,
+        cost1: [2, 4, 7],
+        cost2: null,
+        value: 4,
+        actionType: null
+    },
+    {
+        type: "property",
+        color1: "green",
+        color2: null,
+        cost1: [2, 4, 7],
+        cost2: null,
+        value: 4,
+        actionType: null
+    },
+    {
+        type: "property",
+        color1: "green",
+        color2: null,
+        cost1: [2, 4, 7],
+        cost2: null,
+        value: 4,
+        actionType: null
+    },
+    {
+        type: "property",
+        color1: "orange",
+        color2: null,
+        cost1: [1, 3, 5],
+        cost2: null,
+        value: 2,
+        actionType: null
+    },
+    {
+        type: "property",
+        color1: "orange",
+        color2: null,
+        cost1: [1, 3, 5],
+        cost2: null,
+        value: 2,
+        actionType: null
+    },
+    {
+        type: "property",
+        color1: "orange",
+        color2: null,
+        cost1: [1, 3, 5],
+        cost2: null,
+        value: 2,
+        actionType: null
+    },
+    {
+        type: "property",
+        color1: "black",
+        color2: "green",
+        cost1: [1, 2, 3, 4],
+        cost2: [2, 4, 7],
+        value: 4,
+        actionType: null
+    },
+    {
+        type: "property",
+        color1: "black",
+        color2: null,
+        cost1: [1, 2, 3],
+        cost2: null,
+        value: 2,
+        actionType: null
+    },
+    {
+        type: "property",
+        color1: "black",
+        color2: null,
+        cost1: [1, 2, 3],
+        cost2: null,
+        value: 2,
+        actionType: null
+    },
+    {
+        type: "property",
+        color1: "black",
+        color2: null,
+        cost1: [1, 2, 3],
+        cost2: null,
+        value: 2,
+        actionType: null
+    },
+    {
+        type: "property",
+        color1: "black",
+        color2: null,
+        cost1: [1, 2, 3],
+        cost2: null,
+        value: 2,
+        actionType: null
+    },
+    {
+        type: "property",
+        color1: "lightgreen",
+        color2: null,
+        cost1: [1, 2],
+        cost2: null,
+        value: 2,
+        actionType: null
+    },
+    {
+        type: "property",
+        color1: "lightgreen",
+        color2: null,
+        cost1: [1, 2],
+        cost2: null,
+        value: 2,
+        actionType: null
+    },
+    {
+        type: "property",
+        color1: "any",
+        color2: null,
+        cost1: null,
+        cost2: null,
+        value: null,
+        instructions:"This card can be used as part of any property set. This card has no monetary value.",
+        actionType: null
+    },
+    {
+        type: "property",
+        color1: "any",
+        color2: null,
+        cost1: null,
+        cost2: null,
+        value: null,
+        instructions:"This card can be used as part of any property set. This card has no monetary value.",
+        actionType: null
+    },
+    {
+        type: "money",
+        value: 1
+    },
+    {
+        type: "money",
+        value: 1
+    },
+    {
+        type: "money",
+        value: 1
+    },
+    {
+        type: "money",
+        value: 1
+    },
+    {
+        type: "money",
+        value: 1
+    },
+    {
+        type: "money",
+        value: 1
+    },
+    {
+        type: "money",
+        value: 2
+    },
+    {
+        type: "money",
+        value: 2
+    },
+    {
+        type: "money",
+        value: 2
+    },
+    {
+        type: "money",
+        value: 2
+    },
+    {
+        type: "money",
+        value: 2
+    },
+    {
+        type: "money",
+        value: 3
+    },
+    {
+        type: "money",
+        value: 3
+    },
+    {
+        type: "money",
+        value: 3
+    },
+    {
+        type: "money",
+        value: 4
+    },
+    {
+        type: "money",
+        value: 4
+    },
+    {
+        type: "money",
+        value: 4
+    },
+    {
+        type: "money",
+        value: 5
+    },
+    {
+        type: "money",
+        value: 5
+    },
+    {
+        type: "money",
+        value: 10
+    },
+    {
+        type: "action",
+        actionType: "passGo",
+        display: "Pass GO",
+        value: 1,
+        instructions: "Draw 2 Cards."
+    },
+    {
+        type: "action",
+        actionType: "passGo",
+        display: "Pass GO",
+        value: 1,
+        instructions: "Draw 2 Cards."
+    },
+    {
+        type: "action",
+        actionType: "passGo",
+        display: "Pass GO",
+        value: 1,
+        instructions: "Draw 2 Cards."
+    },
+    {
+        type: "action",
+        actionType: "passGo",
+        display: "Pass GO",
+        value: 1,
+        instructions: "Draw 2 Cards."
+    },
+    {
+        type: "action",
+        actionType: "passGo",
+        display: "Pass GO",
+        value: 1,
+        instructions: "Draw 2 Cards."
+    },
+    {
+        type: "action",
+        actionType: "passGo",
+        display: "Pass GO",
+        value: 1,
+        instructions: "Draw 2 Cards."
+    },
+    {
+        type: "action",
+        actionType: "passGo",
+        display: "Pass GO",
+        value: 1,
+        instructions: "Draw 2 Cards."
+    },
+    {
+        type: "action",
+        actionType: "passGo",
+        display: "Pass GO",
+        value: 1,
+        instructions: "Draw 2 Cards."
+    },
+    {
+        type: "action",
+        actionType: "passGo",
+        display: "Pass GO",
+        value: 1,
+        instructions: "Draw 2 Cards."
+    },
+    {
+        type: "action",
+        actionType: "passGo",
+        display: "Pass GO",
+        value: 1,
+        instructions: "Draw 2 Cards."
+    },
+    {
+        type: "action",
+        actionType: "doubleTheRent",
+        display: "Double The Rent",
+        value: 1,
+        instructions: "Needs to be played with a Rent Card."
+    },
+    {
+        type: "action",
+        actionType: "doubleTheRent",
+        display: "Double The Rent",
+        value: 1,
+        instructions: "Needs to be played with a Rent Card."
+    },
+    {
+        type: "action",
+        actionType: "slyDeal",
+        display: "Sly Deal",
+        value: 3,
+        instructions: "Steal a property from the player of your choice (Cannot be part of a full set)."
+    },
+    {
+        type: "action",
+        actionType: "slyDeal",
+        display: "Sly Deal",
+        value: 3,
+        instructions: "Steal a property from the player of your choice (Cannot be part of a full set)."
+    },
+    {
+        type: "action",
+        actionType: "slyDeal",
+        display: "Sly Deal",
+        value: 3,
+        instructions: "Steal a property from the player of your choice (Cannot be part of a full set)."
+    },
+    {
+        type: "action",
+        actionType: "forcedDeal",
+        display: "Forced Deal",
+        value: 3,
+        instructions: "Swap any property with another player (cannot be part of a full set)."
+    },
+    {
+        type: "action",
+        actionType: "forcedDeal",
+        display: "Forced Deal",
+        value: 3,
+        instructions: "Swap any property with another player (cannot be part of a full set)."
+    },
+    {
+        type: "action",
+        actionType: "forcedDeal",
+        display: "Forced Deal",
+        value: 3,
+        instructions: "Swap any property with another player (cannot be part of a full set)."
+    },
+    {
+        type: "action",
+        actionType: "debtCollector",
+        display: "Debt Collector",
+        value: 3,
+        instructions: "Force any player to pay you $5M."
+    },
+    {
+        type: "action",
+        actionType: "debtCollector",
+        display: "Debt Collector",
+        value: 3,
+        instructions: "Force any player to pay you $5M."
+    },
+    {
+        type: "action",
+        actionType: "debtCollector",
+        display: "Debt Collector",
+        value: 3,
+        instructions: "Force any player to pay you $5M."
+    },
+    {
+        type: "action",
+        actionType: "justSayNo",
+        display: "JUST SAY NO!",
+        value: 4,
+        instructions: "Use at any time when an Action card is played against you."
+    },
+    {
+        type: "action",
+        actionType: "justSayNo",
+        display: "JUST SAY NO!",
+        value: 4,
+        instructions: "Use at any time when an Action card is played against you."
+    },
+    {
+        type: "action",
+        actionType: "justSayNo",
+        display: "JUST SAY NO!",
+        value: 4,
+        instructions: "Use at any time when an Action card is played against you."
+    },
+    {
+        type: "action",
+        actionType: "dealBreaker",
+        display: "Deal Breaker",
+        value: 5,
+        instructions: "Steal a complete set of properties from any player (Includes any buildings)."
+    },
+    {
+        type: "action",
+        actionType: "dealBreaker",
+        display: "Deal Breaker",
+        value: 5,
+        instructions: "Steal a complete set of properties from any player (Includes any buildings)."
+    },
+    {
+        type: "action",
+        actionType: "itsMyBirthday",
+        display: "It's My Birthday",
+        value: 2,
+        instructions: "All players give you $2M as a gift."
+    },
+    {
+        type: "action",
+        actionType: "itsMyBirthday",
+        display: "It's My Birthday",
+        value: 2,
+        instructions: "All players give you $2M as a gift."
+    },
+    
 ]
