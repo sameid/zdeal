3f61b4e03ac7f00c2072a4b1bd2a429d077eff68
sameid <sameid.usmani@gmail.com>
2020-04-11 23:07:44 -0400

    test

---
+++ deck.js
@@ -0,0 +1,200 @@
+let deck = [
+    {
+        type: "property",
+        color1: "yellow",
+        color2: null,
+        cost1: [2, 4, 6],
+        cost2: null,
+        value: 3,
+        actionType: null
+    },
+    {
+        type: "property",
+        color1: "yellow",
+        color2: null,
+        cost1: [2, 4, 6],
+        cost2: null,
+        value: 3,
+        actionType: null
+    },
+    {
+        type: "property",
+        color1: "yellow",
+        color2: null,
+        cost1: [2, 4, 6],
+        cost2: null,
+        value: 3,
+        actionType: null
+    },
+    {
+        type: "property",
+        color1: "yellow",
+        color2: "red",
+        cost1: [2, 4, 6],
+        cost2: [2, 3, 6],
+        value: 3,
+        actionType: null
+    },
+    {
+        type: "property",
+        color1: "yellow",
+        color2: "red",
+        cost1: [2, 4, 6],
+        cost2: [2, 3, 6],
+        value: 3,
+        actionType: null
+    },
+    {
+        type: "property",
+        color1: "red",
+        color2: null,
+        cost1: [2, 3, 6],
+        cost2: null,
+        value: 3,
+        actionType: null
+    },
+    {
+        type: "property",
+        color1: "red",
+        color2: null,
+        cost1: [2, 3, 6],
+        cost2: null,
+        value: 3,
+        actionType: null
+    },
+    {
+        type: "property",
+        color1: "red",
+        color2: null,
+        cost1: [2, 3, 6],
+        cost2: null,
+        value: 3,
+        actionType: null
+    },
+    {
+        type: "property",
+        color1: "lightblue",
+        color2: "black",
+        cost1: [1, 2, 3],
+        cost2: [1, 2, 3, 4],
+        value: 4,
+        actionType: null
+    },
+    {
+        type: "property",
+        color1: "lightblue",
+        color2: null,
+        cost1: [1, 2, 3],
+        cost2: null,
+        value: 1,
+        actionType: null
+    }
+    {
+        type: "property",
+        color1: "lightblue",
+        color2: null,
+        cost1: [1, 2, 3],
+        cost2: null,
+        value: 1,
+        actionType: null
+    }
+    {
+        type: "property",
+        color1: "lightblue",
+        color2: null,
+        cost1: [1, 2, 3],
+        cost2: null,
+        value: 1,
+        actionType: null
+    }
+    {
+        type: "property",
+        color1: "lightgreen",
+        color2: "black",
+        cost1: [1, 2],
+        cost2: [1, 2, 3, 4],
+        value: 2,
+        actionType: null
+    },
+    {
+        type: "property",
+        color1: "pink",
+        color2: null,
+        cost1: [1, 2, 4],
+        cost2: null,
+        value: 2,
+        actionType: null
+    }
+    {
+        type: "property",
+        color1: "pink",
+        color2: null,
+        cost1: [1, 2, 4],
+        cost2: null,
+        value: 2,
+        actionType: null
+    }
+    {
+        type: "property",
+        color1: "pink",
+        color2: null,
+        cost1: [1, 2, 4],
+        cost2: null,
+        value: 2,
+        actionType: null
+    }
+    {
+        type: "property",
+        color1: "pink",
+        color2: "orange",
+        cost1: [1, 2, 4],
+        cost2: [1, 3, 5],
+        value: 2,
+        actionType: null
+    }
+    {
+        type: "property",
+        color1: "pink",
+        color2: "orange",
+        cost1: [1, 2, 4],
+        cost2: [1, 3, 5],
+        value: 2,
+        actionType: null
+    }
+    {
+        type: "property",
+        color1: "brown",
+        color2: null,
+        cost1: [1, 2],
+        cost2: null,
+        value: 1,
+        actionType: null
+    }
+    {
+        type: "property",
+        color1: "brown",
+        color2: null,
+        cost1: [1, 2],
+        cost2: null,
+        value: 1,
+        actionType: null
+    }
+    {
+        type: "property",
+        color1: "brown",
+        color2: "lightblue",
+        cost1: [1, 2],
+        cost2: [1, 2, 3],
+        value: 1,
+        actionType: null
+    }
+    {
+        type: "property",
+        color1: "blue",
+        color2: null,
+        cost1: [3, 8],
+        cost2: null,
+        value: 1,
+        actionType: null
+    }
+]
